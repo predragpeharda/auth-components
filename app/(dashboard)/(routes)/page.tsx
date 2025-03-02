bc9d5c1405cc4f4761e1244aab43acbaba325abb
Predrag Peharda <predragpeharda@Predrags-Laptop.local>
2025-03-02 16:44:41 +0100

    Adding login-button component

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,14 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Protected page</div>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <LoginButton>
+        <Button className="cursor-pointer" size={"lg"}>
+          Sign in
+        </Button>
+      </LoginButton>
+    </main>
+  );
 }
