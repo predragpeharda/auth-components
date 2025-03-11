3a452ba413778cb364e5467f69ded92e0aa12ddc
Predrag Peharda <pehardap@gmail.com>
2025-03-11 20:56:00 +0100

    Create login with credentials server action

--- actions/login.ts
+++ actions/login.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
 import { z } from "zod";
 
@@ -9,5 +12,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
-  return { success: "Email sent" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
