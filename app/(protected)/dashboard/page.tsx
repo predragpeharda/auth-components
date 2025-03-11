73222add794e2f60b58cbab10c6cb92badff938a
Predrag Peharda <pehardap@gmail.com>
2025-03-11 21:16:38 +0100

    Refactor project file names

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { auth, signOut } from "@/auth";
 
-const SettingsPage = async () => {
+const DashboardPage = async () => {
   const session = await auth();
   return (
     <div>
@@ -17,4 +17,4 @@ const SettingsPage = async () => {
   );
 };
 
-export default SettingsPage;
+export default DashboardPage;
