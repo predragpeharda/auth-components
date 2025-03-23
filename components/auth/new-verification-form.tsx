3ec269d346be48bdaaa83312a809ab50ed0b3071
Predrag Peharda <pehardap@gmail.com>
2025-03-23 18:24:22 +0100

    Add email verification

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,17 +1,56 @@
 "use client";
 import { CardWrapper } from "@/components/auth/card-wrapper";
-import { ClipLoader } from "react-spinners";
+import { SpinnerCircular } from "spinners-react";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.succes);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
   return (
     <CardWrapper
-      headingLabel="Confirm your email!"
+      headingLabel="Confirming your email!"
       descriptionLabel=""
       backButtonLabel="Back to login"
       backButtonHref="/auth/login"
     >
       <div className="flex items-center w-full justify-center">
-        <ClipLoader />
+        {!success && !error && (
+          <SpinnerCircular
+            size={35}
+            thickness={120}
+            speed={150}
+            color="#000000"
+            secondaryColor="#f1f1f1"
+          />
+        )}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
